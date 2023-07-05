b0957855416c7ef6f7522586d247ab765973eb0b
bereail <bereailsolohaga@hotmail.com>
2023-07-05 16:26:18 -0300

    DASHBOAR COMPLETO + NEW BOOKLIST

---
+++ src/components/Dashboar/Dashboar.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import FireBase from "../../firebase/FireBase";
+import AddAdminButton from "../AddAdmin/AddAdminButton/AddAdminButton";
+import BookList from "../BookList/BookList";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
+import NewBookButton from "../NewBookButton/NewBookButton";
+
+const Dashboard = () => {
+  const [books, setBooks] = useState([]);
+
+  const addBookHandler = (book) => {
+    const newBooksArray = [book, ...books];
+    setBooks(newBooksArray);
+    // Aquí puedes realizar cualquier otra acción relacionada con el nuevo libro agregado
+  };
+
+  return (
+    <>
+      <Navbar />  
+      <AddAdminButton/>
+      <NewBookButton />
+      <BookList />
+      <FireBase />
+      <Footer />
+    </>
+  );
+};
+
+export default Dashboard;
